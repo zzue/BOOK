fddcd345dc497d15544276506511261ad0248c5e
Wang ZhaoYun <zzustu@163.com>
2018-01-25 13:01:46 +0800

    基本搭建

---
+++ src/main/java/com/gitll/query/filter/StringFilter.java
@@ -0,0 +1,14 @@
+package com.gitll.query.filter;
+
+public class StringFilter extends BaseFilter {
+
+	public StringFilter(String condition) {
+		this.condition = condition;
+	}
+
+	public String getString() {
+		return condition;
+	}
+
+	private final String condition;
+}
